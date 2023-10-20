3cc3df4f29d9acc88ab6aff8a9ee82f4612ab0cf
12leticiasilva0990 <130669115+12leticiasilva0990@users.noreply.github.com>
2023-10-20 10:35:14 -0300

    first commit

---
+++ class.php
@@ -0,0 +1,17 @@
+<?php
+Class Pessoa{
+    public $nome;
+    public $altura;
+    public $nascimento;
+    public $hobbie;
+
+
+public function MostrarPessoa(){
+    echo "Nome: ".$this->nome."<br/>";
+    echo "Altura: ".$this->altura."<br/>";
+    echo "Nascimento: ".$this->nascimento."<br/>";
+    echo "Hobbie: ".$this->hobbie."<br/>";
+}
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,434 @@
+<?php
+//Incluindo a classe que criamos
+include("class.php");
+
+//Instanciando um novo objeto p1
+$p1 = new Pessoa();
+
+//Atribuindo valores ao atributo
+$p1->nome      = "Letícia Gabrielle";
+$p1->altura    = "1.65";
+$p1->nascimento = "30-09-2006";
+$p1->hobbie     = "Ouvir Música e Assistir Série";
+
+//Imprimindo valores inseridos no objeto(p1) usando echo
+echo "Nome: " . $p1->nome . "<br/>
+        Altura: " . $p1->altura . "<br/>
+        Nascimento: " . $p1->nascimento . "<br/>
+        Hobbie: " . $p1->hobbie . "<br/><br/><br/>";
+
+//Instanciando um novo objeto p1
+$p2 = new Pessoa();
+
+//Atribuindo valores ao atributo
+$p2->nome      = "Camilly Vitória";
+$p2->altura    = "1.60";
+$p2->nascimento = "22/01/2007";
+$p2->hobbie     = "Ouvir Música e Fazer aula de Jazz";
+
+
+
+//Instanciando um novo objeto p1
+$p3 = new Pessoa();
+
+//Atribuindo valores ao atributo
+$p3->nome      = "Beatriz Mayumi";
+$p3->altura    = "1.65";
+$p3->nascimento = "03/03/2007";
+$p3->hobbie     = "Ouvir música, ler livros e jogar videogame";
+
+
+//Instanciando um novo objeto p1
+$p4 = new Pessoa();
+
+
+//Atribuindo valores ao atributo
+$p4->nome      = "Pedro Santos";
+$p4->altura    = "1.80";
+$p4->nascimento = "24/10/2006";
+$p4->hobbie     = "Tocar Violão";
+
+
+
+
+//Instanciando um novo objeto p1
+$p5 = new Pessoa();
+
+//Atribuindo valores ao atributo
+$p5->nome      = "Júlia Mogi";
+$p5->altura    = "1.70";
+$p5->nascimento = "08/11/2006";
+$p5->hobbie     = "Cozinhar";
+
+
+
+
+ //Instanciando um novo objeto p1
+$p6 = new Pessoa();
+
+//Atribuindo valores ao atributo
+$p6->nome      = "Laura Perez";
+$p6->altura    = "1.70";
+$p6->nascimento = "03/08/2006";
+$p6->hobbie     = "Ler";
+
+
+
+  //Instanciando um novo objeto p1
+$p8 = new Pessoa();
+
+//Atribuindo valores ao atributo
+$p8->nome      = "Samuel Américo";
+$p8->altura    = "1.85";
+$p8->nascimento = "26/01/2006";
+$p8->hobbie     = "Tocar Piano";
+
+
+
+  //Instanciando um novo objeto p1
+$p9 = new Pessoa();
+
+//Atribuindo valores ao atributo
+$p9->nome      = "Laura Perez";
+$p9->altura    = "1.70";
+$p9->nascimento = "03/08/2006";
+$p9->hobbie     = "Ler";
+
+
+
+
+  //Instanciando um novo objeto p1
+  $p10 = new Pessoa();
+
+  //Atribuindo valores ao atributo
+  $p10->nome      = "Thell Zardi";
+  $p10->altura    = "1.65";
+  $p10->nascimento = "31/10/2006";
+  $p10->hobbie     = "jogar";
+  
+
+
+
+  //Instanciando um novo objeto p1
+  $p11 = new Pessoa();
+
+  //Atribuindo valores ao atributo
+  $p11->nome      = "Kauan Silva Vieira";
+  $p11->altura    = "1.68";
+  $p11->nascimento = "02/09/2006";
+  $p11->hobbie     = "jogar";
+  
+
+     //Instanciando um novo objeto p1
+  $p12 = new Pessoa();
+
+  //Atribuindo valores ao atributo
+  $p12->nome      = "Giovana Lupo";
+  $p12->altura    = "1.62";
+  $p12->nascimento = "14/05/2007";
+  $p12->hobbie     = "Pintar";
+
+  
+     //Instanciando um novo objeto p1
+     $p13 = new Pessoa();
+
+     //Atribuindo valores ao atributo
+     $p13->nome      = "Kauã Lira";
+     $p13->altura    = "1.63";
+     $p13->nascimento = "05/01/2007";
+     $p13->hobbie     = "Tocar Guitarra";
+
+     //Instanciando um novo objeto p1
+     $p14 = new Pessoa();
+
+     //Atribuindo valores ao atributo
+     $p14->nome      = "Henrique Tavares";
+     $p14->altura    = "1.63";
+     $p14->nascimento = "26/07/2006";
+     $p14->hobbie     = "Jogar/ouvir música";
+
+    //Instanciando um novo objeto p1
+    $p15 = new Pessoa();
+
+    //Atribuindo valores ao atributo
+    $p15->nome      = "Ana Zinatto";
+          $p15->altura    = "1.60";
+          $p15->nascimento = "03/11/2006";
+          $p15->hobbie     = "Treinar e ouvir música";
+    
+      //Instanciando um novo objeto p1
+      $p16 = new Pessoa();
+
+      //Atribuindo valores ao atributo
+      $p16->nome      = "Ana Maciel";
+            $p16->altura    = "1.50";
+            $p16->nascimento = "19/01/2007";
+            $p16->hobbie     = "Ler e ouvir música";
+
+      //Instanciando um novo objeto p1
+      $p17 = new Pessoa();
+
+      //Atribuindo valores ao atributo
+      $p17->nome      = "Natally Reis";
+            $p17->altura    = "1.60";
+            $p17->nascimento = "07/01/2007";
+            $p17->hobbie     = "Ouvir música";
+
+      //Instanciando um novo objeto p1
+      $p18 = new Pessoa();
+
+      //Atribuindo valores ao atributo
+      $p18->nome      = "Guilherme Carvalho";
+            $p18->altura    = "1.75";
+            $p18->nascimento = "09./06/2007";
+            $p18->hobbie     = "Jogar";
+
+      //Instanciando um novo objeto p1
+      $p19 = new Pessoa();
+
+      //Atribuindo valores ao atributo
+      $p19->nome      = "Beatriz Baccaro";
+            $p19->altura    = "1.60";
+            $p19->nascimento = "02/08/2006";
+            $p19->hobbie     = "Esporte";
+
+      //Instanciando um novo objeto p1
+      $p20 = new Pessoa();
+
+      //Atribuindo valores ao atributo
+      $p20->nome      = "Pedro Grechi";
+            $p20->altura    = "1.69";
+            $p20->nascimento = "23/03/2007";
+            $p20->hobbie     = "Jogar e Desenhar";
+    
+      //Instanciando um novo objeto p1
+      $p21 = new Pessoa();
+
+      //Atribuindo valores ao atributo
+      $p21->nome      = "Luis Gustavo";
+            $p21->altura    = "1.77";
+            $p21->nascimento = "06/03/2007";
+            $p21->hobbie     = "Dormir e jogar";
+
+    
+     //Instanciando um novo objeto p1
+     $p22 = new Pessoa();
+
+     //Atribuindo valores ao atributo
+     $p22->nome      = "José Luiz";
+           $p22->altura    = "1.71";
+           $p22->nascimento = "15/09/2006";
+           $p22->hobbie     = "Assistir e criticar filme";
+
+     //Instanciando um novo objeto p1
+     $p23 = new Pessoa();
+
+     //Atribuindo valores ao atributo
+     $p23->nome      = "Ryan Silveira";
+           $p23->altura    = "1.72";
+           $p23->nascimento = "02/09/2006";
+           $p23->hobbie     = "Jogar Videogame";
+
+ //Instanciando um novo objeto p1
+ $p24 = new Pessoa();
+
+ //Atribuindo valores ao atributo
+ $p24->nome      = "Gustavo Otaviano";
+       $p24->altura    = "1.85";
+       $p24->nascimento = "04/03/2007";
+       $p24->hobbie     = "Esportes";
+
+ //Instanciando um novo objeto p1
+ $p25 = new Pessoa();
+
+ //Atribuindo valores ao atributo
+ $p25->nome      = "Gabriel Credendio";
+       $p25->altura    = "1.70";
+       $p25->nascimento = "28/07/2006";
+       $p25->hobbie     = "Videogame";
+
+ //Instanciando um novo objeto p1
+ $p26 = new Pessoa();
+
+ //Atribuindo valores ao atributo
+ $p26->nome      = "Rai Franca";
+       $p26->altura    = "1.82";
+       $p26->nascimento = "19/09/2006";
+       $p26->hobbie     = "Esporte, ouvir música e jogos eletrônicos";
+
+ //Instanciando um novo objeto p1
+ $p27 = new Pessoa();
+
+ //Atribuindo valores ao atributo
+ $p27->nome      = "Ana Clara Ferreira";
+       $p27->altura    = "1.55";
+       $p27->nascimento = "21/03/2007";
+       $p27->hobbie     = "Pintar";
+
+ //Instanciando um novo objeto p1
+ $p28 = new Pessoa();
+
+ //Atribuindo valores ao atributo
+ $p28->nome      = "Gabriel Alves";
+       $p28->altura    = "1.84";
+       $p28->nascimento = "03/08/2006";
+       $p28->hobbie     = "jogar videogame e praticar esportes";
+
+//Instanciando um novo objeto p1
+$p29 = new Pessoa();
+
+//Atribuindo valores ao atributo
+$p29->nome      = "Gabriel Diaz";
+      $p29->altura    = "1.74";
+      $p29->nascimento = "21/03/2007";
+      $p29->hobbie     = "jogar";
+
+//Instanciando um novo objeto p1
+$p30 = new Pessoa();
+
+//Atribuindo valores ao atributo
+$p30->nome      = "Duda";
+      $p30->altura    = "1.65";
+      $p30->nascimento = "02/04/2007";
+      $p30->hobbie     = "jogar vôlei";
+
+
+//Instanciando um novo objeto p1
+$p31 = new Pessoa();
+
+//Atribuindo valores ao atributo
+$p31->nome      = "Bruno Alves";
+      $p31->altura    = "1.78";
+      $p31->nascimento = "28/05/2007";
+      $p31->hobbie     = "Tocar Instrumentos";
+
+//Instanciando um novo objeto p1
+$p32 = new Pessoa();
+
+//Atribuindo valores ao atributo
+$p32->nome      = "Felipe Wilbert";
+      $p32->altura    = "1.78";
+      $p32->nascimento = "06/12/2006";
+      $p32->hobbie     = "Academia";
+
+//Instanciando um novo objeto p1
+$p33 = new Pessoa();
+
+//Atribuindo valores ao atributo
+$p33->nome      = "Miguel Romero";
+      $p33->altura    = "1.70";
+      $p33->nascimento = "14/04/2007";
+      $p33->hobbie     = "jogar futebol";
+
+//Instanciando um novo objeto p1
+$p34 = new Pessoa();
+
+//Atribuindo valores ao atributo
+$p34->nome      = "Pedro Araújo";
+      $p34->altura    = "1.68";
+      $p34->nascimento = "24/09/2006";
+      $p34->hobbie     = "jogar video game e andar de bike";
+
+
+      //Instanciando um novo objeto p1
+$p35 = new Pessoa();
+
+//Atribuindo valores ao atributo
+$p35->nome      = "Henrique Nagata";
+      $p35->altura    = "1.58";
+      $p35->nascimento = "25/10/2006";
+      $p35->hobbie     = "andar de bicicleta";
+
+
+
+
+
+
+  
+  
+
+
+
+
+
+//Imprimindo 
+echo "Alunos de Desenvolvimentos de Sistemas do Senai Jacob Lafer:<br/> <br/>";
+$p1->MostrarPessoa();
+echo "<br/> <br/>";
+$p2->MostrarPessoa();
+echo "<br/> <br/>";
+$p3->MostrarPessoa();
+echo "<br/> <br/>";
+$p4->MostrarPessoa();
+echo "<br/> <br/>";
+$p5->MostrarPessoa();
+echo "<br/> <br/>";
+$p6->MostrarPessoa();
+echo "<br/> <br/>";
+echo "<br/> <br/>";
+$p8->MostrarPessoa();
+echo "<br/> <br/>";
+$p9->MostrarPessoa();
+echo "<br/> <br/>";
+$p10->MostrarPessoa();
+echo "<br/> <br/>";
+$p11->MostrarPessoa();
+echo "<br/> <br/>";
+$p12->MostrarPessoa();
+echo "<br/> <br/>";
+$p13->MostrarPessoa();
+echo "<br/> <br/>";
+$p14->MostrarPessoa();
+echo "<br/> <br/>";
+$p15->MostrarPessoa();
+echo "<br/> <br/>";
+$p16->MostrarPessoa();
+echo "<br/> <br/>";
+$p17->MostrarPessoa();
+echo "<br/> <br/>";
+$p18->MostrarPessoa();
+echo "<br/> <br/>";
+$p19->MostrarPessoa();
+echo "<br/> <br/>";
+$p20->MostrarPessoa();
+echo "<br/> <br/>";
+$p21->MostrarPessoa();
+echo "<br/> <br/>";
+$p22->MostrarPessoa();
+echo "<br/> <br/>";
+$p23->MostrarPessoa();
+echo "<br/> <br/>";
+$p24->MostrarPessoa();
+echo "<br/> <br/>";
+$p25->MostrarPessoa();
+echo "<br/> <br/>";
+$p26->MostrarPessoa();
+echo "<br/> <br/>";
+$p27->MostrarPessoa();
+echo "<br/> <br/>";
+$p28->MostrarPessoa();
+echo "<br/> <br/>";
+$p29->MostrarPessoa();
+echo "<br/> <br/>";
+$p30->MostrarPessoa();
+echo "<br/> <br/>";
+$p31->MostrarPessoa();
+echo "<br/> <br/>";
+$p32->MostrarPessoa();
+echo "<br/> <br/>";
+$p33->MostrarPessoa();
+echo "<br/> <br/>";
+$p34->MostrarPessoa();
+echo "<br/> <br/>";
+$p35->MostrarPessoa();
+
+
+
+
+
+
+
+
+
+
+ ?>
